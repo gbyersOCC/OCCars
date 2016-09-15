04591550a09488d240b2bae1ed87b83ab725e124
grant <gbyersOCC>
2016-09-15 10:38:34 -0700

    //

--- app/src/main/java/edu/orangecoastcollege/cs273/gbyers/occars/Car.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/gbyers/occars/Car.java
@@ -8,7 +8,7 @@ public class Car {
     private int mDownPayment;
     private int mLoanTerm;
     private int mPrice;
-
+    double interest;
     public Car()
     {
         mDownPayment = 0;
@@ -30,6 +30,7 @@ public class Car {
     }
 
     public void setLoanTerm(int loanTerm) {
+
         mLoanTerm = loanTerm;
     }
 
@@ -43,19 +44,26 @@ public class Car {
 
     public void calculateBorrowedAmount()
     {
+//is borrowed amount (loan amount minus down payment)
+        // mPrice = mPrice - mDownPayment;
+
 
     }
     public void calculateInterestAmount()
     {
+//I = P*R*T
+        interest = mPrice* (.0308)*mLoanTerm;
 
     }
     public void calculateMonthlyPayment()
     {
+        int months = mLoanTerm*12;
 
+double monthlyAmount = (mPrice*(3.08/1200)*(1+3.08/1200))/((1+3.08/1200)-1);
     }
     public void calculateTaxAmount()
     {
-
+        mPrice*=TAXRATE;
     }
     public void calculateTotalCost()
     {
